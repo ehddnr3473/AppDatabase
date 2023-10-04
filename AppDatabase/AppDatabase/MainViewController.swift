c6c026219ed177481119eb2855be706aca3101ee
김동욱 <ehddnr73@naver.com>
2023-10-05 00:07:47 +0900

    화면 구성

---
+++ AppDatabase/AppDatabase/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  AppDatabase
+//
+//  Created by 김동욱 on 10/4/23.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    @IBOutlet weak var userDefaultsButon: UIButton!
+    @IBOutlet weak var coreDataButton: UIButton!
+    @IBOutlet weak var fileSystemButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func touchUpUserDefaultsButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func touchUpCoreDataButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func touchUpFileSystemButton(_ sender: Any) {
+        
+    }
+}
+
