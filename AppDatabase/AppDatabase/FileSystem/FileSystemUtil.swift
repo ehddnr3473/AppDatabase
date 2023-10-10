3847df9798286829f10696cf86299f287daac7a1
김동욱 <ehddnr73@naver.com>
2023-10-10 23:49:09 +0900

    FileManager를 사용하여 앱 내부 파일 시스템에 데이터 저장하기 구현

--- AppDatabase/AppDatabase/FileSystem/FileSystemUtil.swift
+++ AppDatabase/AppDatabase/FileSystem/FileSystemUtil.swift
@@ -7,9 +7,26 @@
 
 import Foundation
 
+/// FileManager를 사용하여 앱 내부에 데이터를 저장하기 때문에 앱 삭제시 함께 삭제됨.
 struct FileSystemUtil {
     
-    func save() {
-        
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    
+    /// 앱의 Documents 디렉토리에 저장
+    func write(_ fileName: String, _ text: String) {
+        guard let fileUrl = documentDirectory?.appendingPathComponent(fileName, conformingTo: .data) else { return }
+        try? text.data(using: .utf8)?.write(to: fileUrl)
+    }
+    
+    /// 앱의 Documents 디렉토리에서 읽기
+    func read(_ fileName: String) -> String? {
+        guard let fileUrl = documentDirectory?.appendingPathComponent(fileName, conformingTo: .data) else { return nil }
+        return try? String(data: Data(contentsOf: fileUrl), encoding: .utf8)
+    }
+    
+    /// 앱의 Documents 디렉토리에서 삭제
+    func delete(_ fileName: String) {
+        guard let fileUrl = documentDirectory?.appendingPathComponent(fileName, conformingTo: .data) else { return }
+        try? FileManager.default.removeItem(at: fileUrl)
     }
 }
--- AppDatabase/AppDatabase/FileSystem/FileSystemViewController.swift
+++ AppDatabase/AppDatabase/FileSystem/FileSystemViewController.swift
@@ -7,11 +7,48 @@
 
 import UIKit
 
-class FileSystemViewController: UIViewController {
+final class FileSystemViewController: UIViewController {
 
+    @IBOutlet weak var writeFileNameTextField: UITextField!
+    @IBOutlet weak var toBeSavedTextField: UITextField!
+    @IBOutlet weak var writeButton: UIButton!
+    @IBOutlet weak var toBeReadLabel: UILabel!
+    @IBOutlet weak var readFileNameTextField: UITextField!
+    @IBOutlet weak var readButton: UIButton!
+    @IBOutlet weak var toBeDeletedTextField: UITextField!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    private let fileSystemUtil = FileSystemUtil()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpWriteButton() {
+        guard let fileName = writeFileNameTextField.text,
+              let text = toBeSavedTextField.text,
+              fileName != "",
+              text != "" else { return }
+        
+        fileSystemUtil.write(fileName, text)
+        writeFileNameTextField.text = ""
+        toBeSavedTextField.text = ""
+    }
+    
+    @IBAction func touchUpReadButton() {
+        guard let fileName = readFileNameTextField.text else { return }
+        
+        if let readText = fileSystemUtil.read(fileName) {
+            toBeReadLabel.text = readText
+        } else {
+            toBeReadLabel.text = "파일을 찾을 수 없습니다."
+        }
+    }
+    
+    @IBAction func touchUpDeleteButton() {
+        guard let fileName = toBeDeletedTextField.text else { return }
+        
+        fileSystemUtil.delete(fileName)
+        toBeDeletedTextField.text = ""
     }
 }
